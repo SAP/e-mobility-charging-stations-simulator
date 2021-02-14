fc20191350f323b57faa6a9c5eca53a76e2cce55
Jérôme Benoit <jerome.benoit@sap.com>
2021-02-14 20:55:26 +0100

    Minify production build
    And fix GitHub CI.
    
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,22 @@
 import analyze from 'rollup-plugin-analyzer';
 import copy from 'rollup-plugin-copy';
 import del from 'rollup-plugin-delete';
+import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 const isDevelopmentBuild = process.env.BUILD === 'development';
 
 export default {
   input: ['src/start.ts', 'src/charging-station/StationWorker.ts'],
-  output: {
+  output:
+  {
     dir: 'dist',
     format: 'cjs',
     exports: 'auto',
     sourcemap: true,
     preserveModules: true,
-    preserveModulesRoot: 'src'
+    preserveModulesRoot: 'src',
+    ...!isDevelopmentBuild && { plugins: [terser({ numWorkers: 2 })] }
   },
   external: ['crypto', 'perf_hooks', 'fs', 'path', 'poolifier', 'uuid', 'ws', 'winston-daily-rotate-file', 'winston/lib/winston/transports', 'winston', 'worker_threads'],
   plugins: [
