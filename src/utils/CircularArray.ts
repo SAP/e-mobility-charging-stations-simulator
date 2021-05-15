9be37455e6a46ff9c2ab39211fb1c9fb4202d245
Jérôme Benoit <jerome.benoit@sap.com>
2021-05-15 10:02:23 +0200

    CircularArray.ts: Consistent check
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/utils/CircularArray.ts
+++ src/utils/CircularArray.ts
@@ -36,7 +36,7 @@ export class CircularArray<T> extends Array<T> {
     if (concatenatedCircularArray.length > concatenatedCircularArray.size) {
       concatenatedCircularArray.splice(
         0,
-        concatenatedCircularArray.length - this.size
+        concatenatedCircularArray.length - concatenatedCircularArray.size
       );
     }
     return concatenatedCircularArray;
