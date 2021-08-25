9ef75e34cd01ecd7f75ce08ca7e4719d3c2b858b
Jérôme Benoit <jerome.benoit@sap.com>
2021-08-25 23:02:05 +0200

    Fix regexp compliancy for sonar static analyzer
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/utils/performance-storage/JSONFileStorage.ts
+++ src/utils/performance-storage/JSONFileStorage.ts
@@ -10,7 +10,7 @@ export class JSONFileStorage extends Storage {
   }
 
   public storePerformanceStatistics(performanceStatistics: Statistics): void {
-    const performanceJSONFilePath = path.join(path.resolve(__dirname, '../../../'), this.storageURI.pathname.replace(/^\/|\/$/g, ''));
+    const performanceJSONFilePath = path.join(path.resolve(__dirname, '../../../'), this.storageURI.pathname.replace(/(?:^\/)|(?:\/$)/g, ''));
     fs.appendFile(performanceJSONFilePath, JSON.stringify(performanceStatistics, null, 2), 'utf8', (err) => {
       if (err) {
         FileUtils.handleFileException(this.logPrefix, 'Performance measurements', performanceJSONFilePath, err);
