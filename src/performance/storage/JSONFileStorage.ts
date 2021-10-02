1830ad425799159d004ae56f2444f50d8116a935
Jérôme Benoit <jerome.benoit@sap.com>
2021-10-03 01:12:21 +0200

    Indentation cleanup
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- src/performance/storage/JSONFileStorage.ts
+++ src/performance/storage/JSONFileStorage.ts
@@ -23,7 +23,8 @@ export class JSONFileStorage extends Storage {
           const fileData = fs.readFileSync(this.dbName, 'utf8');
           const performanceRecords: Statistics[] = fileData ? JSON.parse(fileData) as Statistics[] : [];
           performanceRecords.push(performanceStatistics);
-          fs.writeFileSync(this.dbName,
+          fs.writeFileSync(
+            this.dbName,
             JSON.stringify(performanceRecords,
               (key, value) => {
                 if (value instanceof Map) {
@@ -35,7 +36,8 @@ export class JSONFileStorage extends Storage {
                 return value as Statistics;
               },
               2),
-            'utf8');
+            'utf8'
+          );
         } catch (error) {
           FileUtils.handleFileException(this.logPrefix, Constants.PERFORMANCE_RECORDS_FILETYPE, this.dbName, error);
         }
