e80008ffa822a339587162458e0b039f5b7c19b8
Jérôme Benoit <jerome.benoit@sap.com>
2022-02-07 14:39:21 +0100

    Add comment for incoming request handling behaviour
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/charging-station/ocpp/OCPPRequestService.ts
+++ src/charging-station/ocpp/OCPPRequestService.ts
@@ -32,6 +32,7 @@ export default abstract class OCPPRequestService {
       }): Promise<JsonType | OCPPError | string> {
     if (this.chargingStation.isInRejectedState() || (this.chargingStation.isInPendingState() && !params.triggerMessage)) {
       throw new OCPPError(ErrorType.SECURITY_ERROR, 'Cannot send command payload if the charging station is not in accepted state', commandName);
+    // FIXME: Add template tunable for accepting incoming ChangeConfiguration request while in unknown state
     } else if ((this.chargingStation.isInUnknownState() && (commandName === RequestCommand.BOOT_NOTIFICATION || commandName === IncomingRequestCommand.CHANGE_CONFIGURATION))
       || this.chargingStation.isInAcceptedState() || (this.chargingStation.isInPendingState() && params.triggerMessage)) {
       // eslint-disable-next-line @typescript-eslint/no-this-alias
