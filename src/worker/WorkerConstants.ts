6f96009b3e4864dcd8771d84d9e826c90018e61c
Jérôme Benoit <jerome.benoit@sap.com>
2022-03-05 20:58:53 +0100

    src/worker/WorkerConstants.ts: add
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

---
+++ src/worker/WorkerConstants.ts
@@ -0,0 +1,8 @@
+export default class WorkerConstants {
+  static readonly DEFAULT_ELEMENT_START_DELAY = 0;
+  static readonly DEFAULT_WORKER_START_DELAY = 500;
+  static readonly POOL_MAX_INACTIVE_TIME = 60000;
+  static readonly DEFAULT_POOL_MIN_SIZE = 4;
+  static readonly DEFAULT_POOL_MAX_SIZE = 16;
+  static readonly DEFAULT_ELEMENTS_PER_WORKER = 1;
+}
