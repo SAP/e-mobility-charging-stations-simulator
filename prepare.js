ef2d0067b8dfaf81922ffb8f1665f4ddff4c30e9
Jérôme Benoit <jerome.benoit@sap.com>
2022-03-09 23:00:21 +0100

    Allow undefined in linter configuration
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- prepare.js
+++ prepare.js
@@ -1,6 +1,4 @@
-// eslint-disable-next-line no-undefined
 const isCi = process.env.CI !== undefined;
-// eslint-disable-next-line no-undefined
 const isCloudFoundry = process.env.VCAP_APPLICATION !== undefined;
 if (!isCloudFoundry && !isCi) {
   // eslint-disable-next-line node/no-unpublished-require
