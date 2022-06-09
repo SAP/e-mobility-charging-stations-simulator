d05d55d8f678618008b480cde67641337cb88a36
HazbinFaulted <lucas.migeon@epitech.eu>
2022-06-09 15:47:01 +0200

    vue js base

---
+++ src/ui/vue/src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import EMobility from '@/composable/EMobility';
+
+const em = EMobility.Instance;
+</script>
---
+++ src/ui/vue/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <ListBorn />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ListBorn from '@/components/ListBorn.vue'; // @ is an alias to /src
+
+</script>
