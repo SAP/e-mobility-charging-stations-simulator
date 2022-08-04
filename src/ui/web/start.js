291880ba001c5c7aac5f3d92bce777f9e5a0bc79
HazbinFaulted <lucas.migeon@epitech.eu>
2022-08-04 11:17:34 +0200

    add rate limiter to local server

--- src/ui/web/start.js
+++ src/ui/web/start.js
@@ -5,6 +5,29 @@ const app = express(),
   PORT = process.env.PORT || 3030,
   uiPath = path.join(__dirname, './dist/');
 
+function rateLimiter(window, max) {
+  const reset = () => setTimeout(() => {
+    console.log('reset');
+    requests = 0;
+    reset();
+  }, window);
+
+  let requests = 0;
+  reset();
+
+  return (req, res, next) => {
+    if (requests > max) {
+      res.sendStatus(429);
+      return;
+    }
+
+    ++requests;
+    next();
+  }
+}
+
+app.use(rateLimiter(10000, 15));
+
 app.use(express.json());
 app.use(express.static(uiPath));
 
