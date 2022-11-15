231d1ecd25f002a28380985f53726d9c8bdd2106
Jérôme Benoit <jerome.benoit@sap.com>
2022-11-15 20:17:38 +0100

    Small integer parsing optimization
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/charging-station/ocpp/OCPPServiceUtils.ts
+++ src/charging-station/ocpp/OCPPServiceUtils.ts
@@ -194,7 +194,8 @@ export class OCPPServiceUtils {
   ): number {
     options.limitationEnabled = options?.limitationEnabled ?? true;
     options.unitMultiplier = options?.unitMultiplier ?? 1;
-    const numberValue = isNaN(parseInt(value)) ? Infinity : parseInt(value);
+    const parsedInt = parseInt(value);
+    const numberValue = isNaN(parsedInt) ? Infinity : parsedInt;
     return options?.limitationEnabled
       ? Math.min(numberValue * options.unitMultiplier, limit)
       : numberValue * options.unitMultiplier;
