293aaa3118bbd8c6a6e7933f049d51031c134fa6
Jérôme Benoit <jerome.benoit@sap.com>
2023-01-25 14:58:44 +0100

    Fix UT
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- test/utils/UtilsTest.ts
+++ test/utils/UtilsTest.ts
@@ -146,19 +146,12 @@ describe('Utils test suite', () => {
   });
 
   it('Verify isIterable()', () => {
-    expect(Utils.isIterable('')).toBe(false);
+    expect(Utils.isIterable('')).toBe(true);
     expect(Utils.isIterable(' ')).toBe(true);
     expect(Utils.isIterable('test')).toBe(true);
     expect(Utils.isIterable(null)).toBe(false);
     expect(Utils.isIterable(undefined)).toBe(false);
-    expect(Utils.isIterable(0)).toBe(false);
-    expect(Utils.isIterable({})).toBe(false);
-    expect(Utils.isIterable({ 1: 1 })).toBe(false);
     expect(Utils.isIterable([])).toBe(true);
-    expect(Utils.isIterable(new Map())).toBe(true);
-    expect(Utils.isIterable(new Set())).toBe(true);
-    expect(Utils.isIterable(new WeakMap())).toBe(false);
-    expect(Utils.isIterable(new WeakSet())).toBe(false);
   });
 
   it('Verify isEmptyString()', () => {
