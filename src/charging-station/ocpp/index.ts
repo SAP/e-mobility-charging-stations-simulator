480fecf6f69bc342772aa161c4452e2b544e12a4
Jérôme Benoit <jerome.benoit@sap.com>
2023-02-14 11:03:48 +0100

    refactor(simulator): sort exports
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/charging-station/ocpp/index.ts
+++ src/charging-station/ocpp/index.ts
@@ -1,12 +1,12 @@
 export {
-  OCPP20IncomingRequestService,
-  OCPP20RequestService,
-  OCPP20ResponseService,
   OCPP16IncomingRequestService,
   OCPP16RequestService,
   OCPP16ResponseService,
   // FIXME: shall not be exported
   OCPP16ServiceUtils,
+  OCPP20IncomingRequestService,
+  OCPP20RequestService,
+  OCPP20ResponseService,
   OCPPIncomingRequestService,
   OCPPRequestService,
   OCPPServiceUtils,
