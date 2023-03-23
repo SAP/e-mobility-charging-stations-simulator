514a30bab4c8c4dbfd9ed06970d081402d005402
Jérôme Benoit <jerome.benoit@sap.com>
2023-03-23 09:26:05 +0100

    build(simulator): add script to prepare a build for CF
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

---
+++ skip-preinstall.cjs
@@ -0,0 +1,7 @@
+if (process.env.SKIP_PREINSTALL) {
+  // eslint-disable-next-line n/no-process-exit
+  process.exit(0);
+} else {
+  // eslint-disable-next-line n/no-process-exit
+  process.exit(1);
+}
