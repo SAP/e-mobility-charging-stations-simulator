67259cdc38294e42febd09c8eefac578920e8e7c
Jérôme Benoit <jerome.benoit@sap.com>
2023-04-25 01:04:15 +0200

    build(ui): enable eslint cache
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- ui/web/.lintstagedrc.js
+++ ui/web/.lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{js,jsx,vue,cjs,mjs,ts,tsx,cts,mts}':
-    'eslint . --ext .vue,.js,.jsx,.cjs,.mjs,.ts,.tsx,.cts,.mts --fix --ignore-path .gitignore',
+    'eslint . --cache --ext .vue,.js,.jsx,.cjs,.mjs,.ts,.tsx,.cts,.mts --fix --ignore-path .gitignore',
     '*.{json,md,yml,yaml}': 'prettier --cache --write',
 };
