884c3516c30c60d8589a4f82e3a42a552b0b2875
Jérôme Benoit <jerome.benoit@sap.com>
2023-04-25 01:11:06 +0200

    build(ui): order lint-staged
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- ui/web/.lintstagedrc.js
+++ ui/web/.lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
+  '*.{json,md,yml,yaml}': 'prettier --cache --write',
   '*.{js,jsx,vue,cjs,mjs,ts,tsx,cts,mts}':
     'eslint . --cache --ext .vue,.js,.jsx,.cjs,.mjs,.ts,.tsx,.cts,.mts --fix --ignore-path .gitignore',
-  '*.{json,md,yml,yaml}': 'prettier --cache --write',
 };
