6fba113357fca89c7df69533d7ffd8ca9bea4f08
Jérôme Benoit <jerome.benoit@sap.com>
2023-04-25 01:05:57 +0200

    refactor(ui): format js files
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- ui/web/.lintstagedrc.js
+++ ui/web/.lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{js,jsx,vue,cjs,mjs,ts,tsx,cts,mts}':
     'eslint . --cache --ext .vue,.js,.jsx,.cjs,.mjs,.ts,.tsx,.cts,.mts --fix --ignore-path .gitignore',
-    '*.{json,md,yml,yaml}': 'prettier --cache --write',
+  '*.{json,md,yml,yaml}': 'prettier --cache --write',
 };
