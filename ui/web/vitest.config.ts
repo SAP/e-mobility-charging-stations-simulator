2de63014d37b8448c73bbe9aa91bdc31ec27b4c1
Jérôme Benoit <jerome.benoit@sap.com>
2023-05-01 21:36:02 +0200

    build(ci): use monorepo setup on sonarcloud
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- ui/web/vitest.config.ts
+++ ui/web/vitest.config.ts
@@ -10,6 +10,10 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      coverage: {
+        provider: 'c8',
+        reporter: ['lcov'],
+      },
     },
   })
 );
