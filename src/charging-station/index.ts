6ae3a6074acfc635d7f8d287e9216446dd1002bb
Jérôme Benoit <jerome.benoit@sap.com>
2023-05-20 17:03:20 +0200

    build(deps-dev): apply updates
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/charging-station/index.ts
+++ src/charging-station/index.ts
@@ -1,3 +1,4 @@
+export { Bootstrap } from './Bootstrap';
 export type { ChargingStation } from './ChargingStation';
 export { ChargingStationConfigurationUtils } from './ChargingStationConfigurationUtils';
 export { ChargingStationUtils } from './ChargingStationUtils';
