60d1f5b09771029bd3459a620e883c58dab0d4f5
Jérôme Benoit <jerome.benoit@sap.com>
2023-07-01 13:03:20 +0200

    feat: use OS optimized pool size by default
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/worker/WorkerConstants.ts
+++ src/worker/WorkerConstants.ts
@@ -1,3 +1,5 @@
+import { availableParallelism } from 'poolifier';
+
 export class WorkerConstants {
   public static readonly EMPTY_FUNCTION = Object.freeze(() => {
     /* This is intentional */
@@ -6,8 +8,8 @@ export class WorkerConstants {
   public static readonly DEFAULT_ELEMENT_START_DELAY = 0;
   public static readonly DEFAULT_WORKER_START_DELAY = 500;
   public static readonly POOL_MAX_INACTIVE_TIME = 60000;
-  public static readonly DEFAULT_POOL_MIN_SIZE = 4;
-  public static readonly DEFAULT_POOL_MAX_SIZE = 16;
+  public static readonly DEFAULT_POOL_MIN_SIZE = availableParallelism() / 2;
+  public static readonly DEFAULT_POOL_MAX_SIZE = availableParallelism();
   public static readonly DEFAULT_ELEMENTS_PER_WORKER = 1;
 
   private constructor() {
