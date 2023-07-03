cc5065999da6bae0caa463848ce4612d65484316
Jérôme Benoit <jerome.benoit@sap.com>
2023-07-03 22:55:04 +0200

    fix: fix availableParallelism() usage for min pool size
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/worker/WorkerConstants.ts
+++ src/worker/WorkerConstants.ts
@@ -8,7 +8,7 @@ export class WorkerConstants {
   public static readonly DEFAULT_ELEMENT_START_DELAY = 0;
   public static readonly DEFAULT_WORKER_START_DELAY = 500;
   public static readonly POOL_MAX_INACTIVE_TIME = 60000;
-  public static readonly DEFAULT_POOL_MIN_SIZE = availableParallelism() / 2;
+  public static readonly DEFAULT_POOL_MIN_SIZE = Math.floor(availableParallelism() / 2);
   public static readonly DEFAULT_POOL_MAX_SIZE = availableParallelism();
   public static readonly DEFAULT_ELEMENTS_PER_WORKER = 1;
 
