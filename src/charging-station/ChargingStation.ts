d116b9b5f418a020bc6f971580a23a04cdf8497a
Jérôme Benoit <jerome.benoit@sap.com>
2023-09-06 00:10:35 +0200

    refactor: use helper for null in undefined in condition
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/charging-station/ChargingStation.ts
+++ src/charging-station/ChargingStation.ts
@@ -1033,7 +1033,7 @@ export class ChargingStation {
   }
 
   private stopReservationExpirationSetInterval(): void {
-    if (this.reservationExpirationSetInterval) {
+    if (!isNullOrUndefined(this.reservationExpirationSetInterval)) {
       clearInterval(this.reservationExpirationSetInterval);
     }
   }
