69d1cb501a021bc921127f7e4e91ba6dcfaccf66
Jérôme Benoit <jerome.benoit@sap.com>
2023-09-22 20:44:11 +0200

    build: refine husky configuration
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- skip-preinstall.cjs
+++ skip-preinstall.cjs
@@ -1,7 +1,7 @@
 const skipPreinstall = process.env.SKIP_PREINSTALL || process.env.VCAP_APPLICATION !== undefined;
 if (skipPreinstall) {
   // eslint-disable-next-line n/no-process-exit
-  process.exit(0);
+  process.exit();
 } else {
   // eslint-disable-next-line n/no-process-exit
   process.exit(1);
