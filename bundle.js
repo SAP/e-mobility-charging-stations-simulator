44ebef4c21047693fbc194aa9b7f90578465e29a
Jérôme Benoit <jerome.benoit@sap.com>
2023-10-30 13:45:25 +0100

    build: rollup -> esbuild
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

---
+++ bundle.js
@@ -0,0 +1,54 @@
+/* eslint-disable n/no-unpublished-import */
+import { env } from 'node:process';
+
+import { build } from 'esbuild';
+import { clean } from 'esbuild-plugin-clean';
+import { copy } from 'esbuild-plugin-copy';
+
+const isDevelopmentBuild = env.BUILD === 'development';
+const sourcemap = !!isDevelopmentBuild;
+
+(async () => {
+  await build({
+    entryPoints: ['./src/start.ts', './src/charging-station/ChargingStationWorker.ts'],
+    bundle: true,
+    platform: 'node',
+    format: 'esm',
+    external: ['@mikro-orm/*'],
+    minify: true,
+    sourcemap,
+    outdir: './dist',
+    plugins: [
+      clean({
+        patterns: [
+          './dist/*',
+          '!./dist/assets',
+          './dist/assets/*.json',
+          './dist/assets/json-schemas',
+          './dist/assets/station-templates',
+          './dist/assets/ui-protocol',
+        ],
+      }),
+      copy({
+        assets: [
+          {
+            from: ['./src/assets/config.json'],
+            to: ['./assets'],
+          },
+          {
+            from: ['./src/assets/idtags!(-template)*.json'],
+            to: ['./assets'],
+          },
+          {
+            from: ['./src/assets/json-schemas/**/*.json'],
+            to: ['./assets/json-schemas'],
+          },
+          {
+            from: ['./src/assets/station-templates/**/*.json'],
+            to: ['./assets/station-templates'],
+          },
+        ],
+      }),
+    ],
+  });
+})();
