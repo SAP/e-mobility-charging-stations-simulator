ca0da0da5f4dc739572c835d6e53e40de3a42382
Jérôme Benoit <jerome.benoit@sap.com>
2023-12-11 13:49:21 +0100

    perf: a charging station worker is not an AsyncResource
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/charging-station/ChargingStationWorker.ts
+++ src/charging-station/ChargingStationWorker.ts
@@ -1,6 +1,5 @@
 // Partial Copyright Jerome Benoit. 2021-2023. All Rights Reserved.
 
-import { AsyncResource } from 'node:async_hooks';
 import { parentPort } from 'node:worker_threads';
 
 import { ThreadWorker } from 'poolifier';
@@ -9,9 +8,7 @@ import { ChargingStation } from './ChargingStation';
 import { BaseError } from '../exception';
 import type { ChargingStationWorkerData } from '../types';
 import { Configuration } from '../utils';
-import { type WorkerData, type WorkerMessage, WorkerMessageEvents } from '../worker';
-
-const moduleName = 'ChargingStationWorker';
+import { type WorkerMessage, WorkerMessageEvents } from '../worker';
 
 /**
  * Creates and starts a charging station instance
@@ -22,19 +19,14 @@ const startChargingStation = (data?: ChargingStationWorkerData): void => {
   new ChargingStation(data!.index, data!.templateFile).start();
 };
 
-class ChargingStationWorker<Data extends WorkerData> extends AsyncResource {
+class ChargingStationWorker<Data extends ChargingStationWorkerData> {
   constructor() {
-    super(moduleName);
     // Add message listener to create and start charging station from the main thread
     parentPort?.on('message', (message: WorkerMessage<Data>) => {
       switch (message.event) {
         case WorkerMessageEvents.startWorkerElement:
           try {
-            this.runInAsyncScope(
-              startChargingStation.bind(this) as (data?: Data) => void,
-              this,
-              message.data,
-            );
+            startChargingStation(message.data);
             parentPort?.postMessage({
               event: WorkerMessageEvents.startedWorkerElement,
             });
