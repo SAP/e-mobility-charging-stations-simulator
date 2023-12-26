9eb4b50a520573fd3342f7a924899523c34ec16e
Jérôme Benoit <jerome.benoit@sap.com>
2023-12-26 20:18:18 +0100

    build(ui): report coverage on console
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- ui/web/vitest.config.ts
+++ ui/web/vitest.config.ts
@@ -12,7 +12,7 @@ export default mergeConfig(
       root: fileURLToPath(new URL('./', import.meta.url)),
       coverage: {
         provider: 'v8',
-        reporter: ['lcov'],
+        reporter: ['text', 'lcov'],
       },
     },
   }),
