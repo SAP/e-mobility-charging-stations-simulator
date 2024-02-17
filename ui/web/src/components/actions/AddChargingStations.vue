7086aac2294163414002bc34bab83acf515a5d37
Jérôme Benoit <jerome.benoit@sap.com>
2024-02-17 22:51:09 +0100

    fix(ci): add missing .vue file
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

---
+++ ui/web/src/components/actions/AddChargingStations.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>Action Add Charging Stations</h2>
+</template>
+
+<script setup lang="ts">
+import { getCurrentInstance, onMounted } from 'vue'
+
+const app = getCurrentInstance()
+const uiClient = app?.appContext.config.globalProperties.$uiClient
+
+onMounted(() => {
+  uiClient.listTemplates().then((response: ResponsePayload) => {
+    if (app != null && app.appContext.config.globalProperties.$templates == null) {
+      app.appContext.config.globalProperties.$templates = response.templates
+    }
+  })
+})
+</script>
+
+<style></style>
