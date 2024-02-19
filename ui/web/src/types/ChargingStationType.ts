5c51245316333bee6d39658710c40f5b40d68643
Jérôme Benoit <jerome.benoit@sap.com>
2024-02-19 15:17:34 +0100

    chore(ui): prepare for 0.2.0 release
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- ui/web/src/types/ChargingStationType.ts
+++ ui/web/src/types/ChargingStationType.ts
@@ -213,7 +213,8 @@ type CommandsSupport = {
 
 export enum OCPPVersion {
   VERSION_16 = '1.6',
-  VERSION_20 = '2.0'
+  VERSION_20 = '2.0',
+  VERSION_201 = '2.0.1'
 }
 
 export enum OCPPProtocol {
