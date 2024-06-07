2a9305b5ace7b5a49b5862c4282c1a88e3087da5
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-06-07 13:36:22 +0200

    test: add ConfigurationUtils test
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

---
+++ tests/utils/ConfigurationUtils.test.ts
@@ -0,0 +1,16 @@
+import { describe, it } from 'node:test'
+
+import { expect } from 'expect'
+
+import { FileType } from '../../src/types/index.js'
+import { handleFileException } from '../../src/utils/ConfigurationUtils.js'
+
+await describe('ConfigurationUtils test suite', async () => {
+  await it('Verify handleFileException()', () => {
+    const error = new Error()
+    error.code = 'ENOENT'
+    expect(() => {
+      handleFileException('path/to/module.js', FileType.Authorization, error, 'log prefix')
+    }).toThrow(error)
+  })
+})
