b49550e256d11c7a30fb19cd672e5e3611d01553
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-06-07 14:04:44 +0200

    test: add tests
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

---
+++ tests/exception/OCPPError.test.ts
@@ -0,0 +1,23 @@
+import { describe, it } from 'node:test'
+
+import { expect } from 'expect'
+
+import { OCPPError } from '../../src/exception/OCPPError.js'
+import { ErrorType } from '../../src/types/index.js'
+import { Constants } from '../../src/utils/Constants.js'
+
+await describe('OCPPError test suite', async () => {
+  await it('Verify that OCPPError can be instantiated', () => {
+    const ocppError = new OCPPError(ErrorType.GENERIC_ERROR, '')
+    expect(ocppError).toBeInstanceOf(OCPPError)
+    expect(ocppError.name).toBe('OCPPError')
+    expect(ocppError.message).toBe('')
+    expect(ocppError.code).toBe(ErrorType.GENERIC_ERROR)
+    expect(ocppError.command).toBe(Constants.UNKNOWN_OCPP_COMMAND)
+    expect(ocppError.details).toBeUndefined()
+    expect(typeof ocppError.stack === 'string').toBe(true)
+    expect(ocppError.stack).not.toBe('')
+    expect(ocppError.cause).toBeUndefined()
+    expect(ocppError.date).toBeInstanceOf(Date)
+  })
+})
--- tests/utils/ErrorUtils.test.ts
+++ tests/utils/ErrorUtils.test.ts
@@ -2,9 +2,23 @@ import { describe, it } from 'node:test'
 
 import { expect } from 'expect'
 
-import { setDefaultErrorParams } from '../../src/utils/ErrorUtils.js'
+import { FileType } from '../../src/types/index.js'
+import { handleFileException, setDefaultErrorParams } from '../../src/utils/ErrorUtils.js'
 
 await describe('ErrorUtils test suite', async () => {
+  await it('Verify handleFileException()', () => {
+    const error = new Error()
+    error.code = 'ENOENT'
+    expect(() => {
+      handleFileException('path/to/module.js', FileType.Authorization, error, 'log prefix', {})
+    }).toThrow(error)
+    expect(() => {
+      handleFileException('path/to/module.js', FileType.Authorization, error, 'log prefix', {
+        throwError: false
+      })
+    }).not.toThrow()
+  })
+
   await it('Verify setDefaultErrorParams()', () => {
     expect(setDefaultErrorParams({})).toStrictEqual({ throwError: true, consoleOut: false })
     expect(setDefaultErrorParams({ throwError: false })).toStrictEqual({
