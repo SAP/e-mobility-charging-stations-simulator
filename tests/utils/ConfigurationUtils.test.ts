8598b5618e5acf6b9e2538faa5225ba51aa68d10
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-06-07 18:15:30 +0200

    test: improve ConfigurationUtils coverage
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- tests/utils/ConfigurationUtils.test.ts
+++ tests/utils/ConfigurationUtils.test.ts
@@ -4,9 +4,13 @@ import { describe, it } from 'node:test'
 import { expect } from 'expect'
 
 import { FileType } from '../../src/types/index.js'
-import { handleFileException } from '../../src/utils/ConfigurationUtils.js'
+import { handleFileException, logPrefix } from '../../src/utils/ConfigurationUtils.js'
 
 await describe('ConfigurationUtils test suite', async () => {
+  await it('Verify logPrefix()', () => {
+    expect(logPrefix()).toContain(' Simulator configuration |')
+  })
+
   await it('Verify handleFileException()', t => {
     t.mock.method(console, 'error')
     const error = new Error()
