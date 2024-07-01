47a41cfff7de841a931f5694dbcddaf235e91bdd
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-07-02 00:29:09 +0200

    test: add ConfigurationData enums tests
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

---
+++ tests/types/ConfigurationData.test.ts
@@ -0,0 +1,29 @@
+import { describe, it } from 'node:test'
+
+import { expect } from 'expect'
+
+import {
+  ApplicationProtocolVersion,
+  ConfigurationSection,
+  SupervisionUrlDistribution
+} from '../../src/types/ConfigurationData.js'
+
+await describe('ConfigurationData test suite', async () => {
+  await it('Verify ConfigurationSection enumeration', () => {
+    expect(ConfigurationSection.log).toBe('log')
+    expect(ConfigurationSection.performanceStorage).toBe('performanceStorage')
+    expect(ConfigurationSection.worker).toBe('worker')
+    expect(ConfigurationSection.uiServer).toBe('uiServer')
+  })
+
+  await it('Verify SupervisionUrlDistribution enumeration', () => {
+    expect(SupervisionUrlDistribution.ROUND_ROBIN).toBe('round-robin')
+    expect(SupervisionUrlDistribution.RANDOM).toBe('random')
+    expect(SupervisionUrlDistribution.CHARGING_STATION_AFFINITY).toBe('charging-station-affinity')
+  })
+
+  await it('Verify ApplicationProtocolVersion enumeration', () => {
+    expect(ApplicationProtocolVersion.VERSION_11).toBe('1.1')
+    expect(ApplicationProtocolVersion.VERSION_20).toBe('2.0')
+  })
+})
