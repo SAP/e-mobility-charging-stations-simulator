5db70814a94ebcf0286243685f0848d23493b32a
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-10-27 00:09:24 +0200

    chore: fix lint-staged configuration
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- ui/web/.lintstagedrc.js
+++ ui/web/.lintstagedrc.js
@@ -1,4 +1,4 @@
 export default {
-  '*.{.css,json,md,yml,yaml,html,js,jsx,cjs,mjs,ts,tsx,cts,mts}': 'prettier --cache --write',
+  '*.{css,json,md,yml,yaml,html,js,jsx,cjs,mjs,ts,tsx,cts,mts}': 'prettier --cache --write',
   '*.{vue,js,jsx,cjs,mjs,ts,tsx,cts,mts}': 'eslint --cache --fix',
 }
